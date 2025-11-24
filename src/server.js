d495bde3a45c31491afbebb4774c955850c69e0e
Hemanth <rachapalli.hemanth5544@gmail.com>
2025-11-24 15:35:12 +0530

    scalar docs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/database');
+const { apiReference } =require ('@scalar/express-api-reference');
+const path = require('path');
+app.use('/openapi.json', express.static(path.join(__dirname, '../openapi.json')));
+
+app.use(
+  '/reference',
+  apiReference({
+    theme: 'purple',
+    url: '/openapi.json',
+  })
+);
+
+
 
 const app = express();
 
@@ -12,6 +25,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`REST API: http://localhost:${PORT}/api`);
+  console.log(`REST API: http://localhost:${PORT}/reference`);
   console.log(`GraphQL API: http://localhost:${PORT}/graphql`);
 });
 
